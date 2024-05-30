c9707b4bb893e4fdc22971fe49cbfba8b429c688
alfredoperry14 <alfredoperry14@gmail.com>
2024-05-29 19:15:10 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Alfredo Perry on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
